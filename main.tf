72ad3daac84e53cc0efd99566aa5b86611163a7a
whuandra <50607920+whuandra@users.noreply.github.com>
2020-08-13 21:04:44 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "win_ec2" {
   ami = "ami-02354e95b39ca8dec"
   instance_type = "${var.instancetype}"
   tags = {
-    Name = "${var.instancename"}
+    Name = "${var.instancename}"
    }
 }
